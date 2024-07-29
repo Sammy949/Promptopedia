dff3531b5977402836080c57148ce55589435730
Sammy949 <urahsamuel0202@gmail.com>
2024-07-30 00:22:08 +0100

    Static Fix 4 - One more time

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,20 +1,14 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
-import { NextRequest, NextResponse } from "next/server";
-import { revalidatePath } from "next/cache";
 
+export const dynamic = "force-dynamic";
 export const GET = async (request) => {
   try {
-    await connectToDatabase();
+    await connectToDB();
 
     const prompts = await Prompt.find({}).populate("creator");
 
-    //To dynamically get the path
-    const path = request.nextUrl.searchParams.get("path") || "/";
-
-    revalidatePath(path);
-
-    return NextResponse.json(prompts);
+    return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
