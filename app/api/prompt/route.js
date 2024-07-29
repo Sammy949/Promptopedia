0cbd5c702b404eea7f1de926521ba88444b0bbf2
Sammy949 <urahsamuel0202@gmail.com>
2024-07-30 00:10:03 +0100

    reverting changes

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,13 +1,12 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
-export const revalidate = 1; //revalidate api every 1 second
 export const GET = async (req) => {
   try {
     await connectToDB();
 
     const prompts = await Prompt.find({}).populate("creator");
-
+    
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
